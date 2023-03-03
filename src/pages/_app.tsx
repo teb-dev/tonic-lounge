5f28f899997876a82d46526beb893e482ae98619
nahee-park <jessica396@ewhain.net>
2023-03-04 02:18:10 +0900

    dev: update package and setting emotion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import '@src/styles/globals.css';
-
+import { Global, ThemeProvider } from '@emotion/react';
+import { global } from '@src/styles/global';
+import theme from '@src/styles/theme';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
