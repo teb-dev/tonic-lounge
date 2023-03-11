831b19b1fe128292ed01990de65e57c7b351fe5c
nahee-park <jessica396@ewhain.net>
2023-03-11 17:15:38 +0900

    feat: image s3 upload api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,32 @@
 import { LoungeResponse } from '@src/types';
 import axios from 'axios';
 const URL = 'http://ec2-3-37-70-35.ap-northeast-2.compute.amazonaws.com:3000';
+const BUCKET_URL = 'https://tonic-lounge-nft.s3.ap-northeast-2.amazonaws.com/';
 
 export const getLounges = async (page: number) => {
   const { data }: { data: LoungeResponse } = await axios.get(`${URL}/api/lounges?page=${page}`);
 
   return data;
+};
+
+export const postImageToS3 = async (file: File) => {
+  const { data } = await axios.post('/api/s3/uploadFile', {
+    name: file.name,
+    type: file.type,
+  });
+
+  console.log('data', data);
+
+  const { url } = data;
+
+  const { data: newData } = await axios.put(url, file, {
+    headers: {
+      'Content-type': file.type,
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
+
+  console.log('newData', newData);
+
+  return BUCKET_URL + file.name;
 };
